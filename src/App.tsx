ea108aac617b4c2ac1f6559fc5cc94f059de3ca4
RMS <rene.as.himself@gmail.com>
2019-09-30 14:52:55 -0500

    Constant files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import TextField from './components/TextField';
 
 const App: React.FC = () => {
   return (
     <div>
-        <TextField text='hello' person={{firstName: 'Bob', lastName: 'Smith'}}
-        handleChange={e => {
-            console.log(`${e.target}`)
-        }}></TextField>
+
     </div>
   );
 }
